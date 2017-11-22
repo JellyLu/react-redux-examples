d35d0580a16eed824c195298270142ad9ec5aed2
JellyLu <634725546@qq.com>
2017-11-22 18:11:08 +0800

    init sample

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import config from './config.json';
+import styles from './header.css';
+
+class Header extends Component{
+  render() {
+    return (
+      <div className={styles.headerRoot}>
+        {config.message}
+      </div>
+    );
+  }
+}
+
+export default Header;
